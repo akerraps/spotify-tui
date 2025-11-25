b823639275a0367cf000623dd36a3e2c0e9d262f
akerraps <akerraps@gmail.com>
2025-11-25 22:14:59 +0100

    Add playlist list

--- main.go
+++ main.go
@@ -18,10 +18,16 @@ func main() {
 	authenticate.InitAuth()
 	client := authenticate.StartLogin()
 
+	ctx := context.Background()
 	// use the client to make calls that require authorization
-	user, err := client.CurrentUser(context.Background())
+	user, err := client.CurrentUser(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("You are logged in as:", user.ID)
+
+	playlists, _ := client.CurrentUsersPlaylists(ctx)
+	for _, p := range playlists.Playlists {
+		fmt.Println("Playlist:", p.Name)
+	}
 }
