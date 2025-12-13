76dc2154ee442cd676e64d8160b8ec28e878c8c5
akerraps <akerraps@gmail.com>
2025-12-13 20:00:27 +0100

    Add cli commands

--- cmd/tunctl/main.go
+++ cmd/tunctl/main.go
@@ -4,22 +4,33 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
-	authenticate "akerraps/tunectl/internal/authenticate"
+	"akerraps/tunectl/internal/authenticate"
 	playlists "akerraps/tunectl/internal/spotify"
 
 	"github.com/joho/godotenv"
+	"github.com/urfave/cli/v2"
 )
 
 func init() {
 	godotenv.Load()
 }
 
-func main() {
+type App struct {
+	Name string
+}
+
+func NewApp(ctx context.Context) *App {
+	return &App{
+		Name: "Tunectl",
+	}
+}
+
+func (a *App) RunTasks(ctx context.Context) error {
 	authenticate.InitAuth()
 	client := authenticate.StartLogin()
 
-	ctx := context.Background()
 	// use the client to make calls that require authorization
 	user, err := client.CurrentUser(ctx)
 	if err != nil {
@@ -29,10 +40,48 @@ func main() {
 
 	myPlaylists := playlists.ListPlaylists(ctx, client)
 
-	for _, p := range myPlaylists.Playlists {
-		playlistID := p.ID
-		myPlaylistData := playlists.PlaylistData(ctx, client, playlistID)
-		myTrackInfo := playlists.PlaylistTracks(myPlaylistData)
-		fmt.Println(myTrackInfo)
-	}
+	fmt.Println(myPlaylists)
+	return nil
 }
+
+func main() {
+	ctx := context.Background()
+
+	appCore := NewApp(ctx)
+
+	cliApp := &cli.App{
+		Name: "Tunectl",
+		Commands: []*cli.Command{
+			{
+				Name: "playlists",
+				Action: func(c *cli.Context) error {
+					return appCore.RunTasks(c.Context)
+				},
+			},
+		},
+	}
+
+	cliApp.RunContext(ctx, os.Args)
+}
+
+// func main() {
+// 	authenticate.InitAuth()
+// 	client := authenticate.StartLogin()
+//
+// 	ctx := context.Background()
+// 	// use the client to make calls that require authorization
+// 	user, err := client.CurrentUser(ctx)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println("You are logged in as:", user.ID)
+//
+// 	myPlaylists := playlists.ListPlaylists(ctx, client)
+//
+// 	for _, p := range myPlaylists.Playlists {
+// 		playlistID := p.ID
+// 		myPlaylistData := playlists.PlaylistData(ctx, client, playlistID)
+// 		myTrackInfo := playlists.PlaylistTracks(myPlaylistData)
+// 		fmt.Println(myTrackInfo)
+// 	}
+// }
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module akerraps/tunectl
 go 1.25.4
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.27.7 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	github.com/zmb3/spotify v1.3.0 // indirect
 	github.com/zmb3/spotify/v2 v2.4.3 // indirect
 	golang.org/x/oauth2 v0.33.0 // indirect
