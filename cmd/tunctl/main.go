7b099ae49cfe31dcecae914f9b658cdf460151f3
akerraps <akerraps@gmail.com>
2025-12-14 14:57:09 +0100

    Make songs cli command work

--- cmd/tunctl/main.go
+++ cmd/tunctl/main.go
@@ -53,8 +53,7 @@ func main() {
 
 					playlist := c.Args().Get(0)
 
-					fmt.Println("Playlist", playlist)
-					return nil
+					return appCore.RunSogns(c.Context, playlist)
 				},
 			},
 		},
@@ -85,6 +84,21 @@ func (a *App) RunPlaylists(ctx context.Context) error {
 
 	for _, p := range myPlaylists.Playlists {
 		fmt.Printf("Playlist found: %s\n", p.Name)
+	}
+	return nil
+}
+
+func (a *App) RunSogns(ctx context.Context, playlistName string) error {
+	client := auth(ctx)
+	myPlaylists := playlists.ListPlaylists(ctx, client)
+
+	for _, p := range myPlaylists.Playlists {
+		if playlistName == p.Name {
+			playlistID := p.ID
+			myPlaylistData := playlists.PlaylistData(ctx, client, playlistID)
+			myTrackInfo := playlists.PlaylistTracks(myPlaylistData)
+			fmt.Println(myTrackInfo)
+		}
 	}
 	return nil
 }
