362dd5582ff4fc7a436671a2fb1ad158407df0fe
akerraps <akerraps@gmail.com>
2026-01-01 23:27:43 +0100

    Add check if a song exists

--- internal/fetcher/fetcher.go
+++ internal/fetcher/fetcher.go
@@ -31,7 +31,7 @@ func checkDependencies() (string, error) {
 			return "", fmt.Errorf("cannot create cache dir: %w", err)
 		}
 
-		if err := getter.GetAny(cachePath, "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp"); err != nil {
+		if err := getter.GetAny(cachePath, "https://github.com/yt-dlp/yt-dlp/releases/latest/diownload/yt-dlp"); err != nil {
 			return "", fmt.Errorf("failed to download yt-dlp: %w", err)
 		}
 
@@ -41,6 +41,14 @@ func checkDependencies() (string, error) {
 	}
 
 	return path, nil
+}
+
+func songExists(prefix string) (bool, error) {
+	matches, err := filepath.Glob(prefix + ".*")
+	if err != nil {
+		return false, err
+	}
+	return len(matches) > 0, nil
 }
 
 func FetchAudio(tracks []types.TrackInfo, out string) error {
@@ -54,19 +62,36 @@ func FetchAudio(tracks []types.TrackInfo, out string) error {
 		name := song.Name
 		artist := strings.Join(song.Artists, " ")
 		output := filepath.Join(out, name)
+
+		exists, err := songExists(output)
+		if err != nil {
+			return err
+		}
+
+		if exists {
+			log.Printf("already exists: %s - %s", name, artist)
+			continue
+		}
+
+		log.Printf("fetching %s - %s", name, artist)
 		cmd := exec.Command(bin,
 			"-x",
+			"--restrict-filenames",
+			"--windows-filenames",
+			"--quiet",
+			"--no-warnings",
 			"-f", "bestaudio",
 			"ytsearch:"+name+" "+artist,
 			"-o", output)
 
-		stdout, err := cmd.Output()
+		_, err = cmd.Output()
+
 		if err != nil {
 			log.Printf("failed to fetch %s - %s: %v", name, artist, err)
 			continue
+		} else {
+			log.Printf("fetched %s - %s", name, artist)
 		}
-
-		log.Println(string(stdout))
 	}
 
 	return nil
