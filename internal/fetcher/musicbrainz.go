3aae8b56cff57c9ae876bf623c0fb6cce946eb33
akerraps <akerraps@gmail.com>
2026-05-17 21:43:21 +0200

    Add music genre support

--- internal/fetcher/musicbrainz.go
+++ internal/fetcher/musicbrainz.go
@@ -7,6 +7,11 @@ import (
 	"github.com/michiwend/gomusicbrainz"
 )
 
+type Tag struct {
+	Count int
+	Name  string
+}
+
 func createClient() (client *gomusicbrainz.WS2Client) {
 	client, _ = gomusicbrainz.NewWS2Client(
 		"https://musicbrainz.org/ws/2",
@@ -17,9 +22,10 @@ func createClient() (client *gomusicbrainz.WS2Client) {
 	return client
 }
 
-func GetSongInfo(song string, artist string) (string, string, error) {
+func GetSongInfo(song string, artist string) (songName string, artistName string, err error) {
 	client := createClient()
 	query := fmt.Sprintf(`recording:"%s" AND artist:%s`, song, artist)
+	artistId := ""
 
 	resp, err := client.SearchRecording(query, 1, 0)
 	if err != nil {
@@ -27,13 +33,32 @@ func GetSongInfo(song string, artist string) (string, string, error) {
 	}
 
 	if len(resp.Recordings) == 0 {
-		log.Printf("no information found por song \"%s\" and artist \"%s\"\n", song, artist)
+		log.Printf("no information found for song \"%s\" and artist \"%s\"\n", song, artist)
 	} else {
 
 		song = resp.Recordings[0].Title
 		artist = resp.Recordings[0].ArtistCredit.NameCredits[0].Artist.Name
-
+		artistId = string(resp.Recordings[0].ArtistCredit.NameCredits[0].Artist.ID)
+		fmt.Println(getArtistInfo(artistId))
 	}
 
 	return song, artist, err
+}
+
+func getArtistInfo(artistID string) ([]string, error) {
+	client := createClient()
+
+	resp, err := client.SearchArtist("arid:"+artistID, 1, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	tags := resp.Artists[0].Tags
+
+	names := make([]string, 0, len(tags))
+	for _, tag := range tags {
+		names = append(names, tag.Name)
+	}
+
+	return names, nil
 }
